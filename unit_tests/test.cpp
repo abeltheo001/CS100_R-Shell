0b41334a545b70a0f2335c12a78d7fef9110a5cb
OrderFromChaos <snore001@ucr.edu>
2020-01-20 18:35:10 -0800

    Added additional tests for splitSpaces.h

--- unit_tests/test.cpp
+++ unit_tests/test.cpp
@@ -20,6 +20,29 @@ TEST (splitSpaces, singleSpace) {
 	EXPECT_EQ(values, outputV);
 }
 	
+TEST (splitSpaces, flankingSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("              abc 123              ");
+	EXPECT_EQ(values, outputV);
+}
+
+TEST (splitSpaces, multipleMiddleSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("abc             123");
+	EXPECT_EQ(values, outputV);
+}
+
+TEST (splitSpaces, allSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("             abc               123               ");
+	EXPECT_EQ(values, outputV);
+}
 
 
 int main(int argc, char **argv) {
