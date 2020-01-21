02757bec7d1229abc036a2be53cdc2fd562ef899
Abel Theodros <atheo003@hammer.cs.ucr.edu>
2020-01-20 17:21:35 -0800

    update test.cpp

--- unit_tests/test.cpp
+++ unit_tests/test.cpp
@@ -1,5 +1,13 @@
 #include "gtest/gtest.h"
 
+TEST (hashParseTest, FirstCheck)
+{
+	vector <string> values;
+	values.push_back("hamina");
+	values.push_back("#");	
+	EXPECT_EQ("hamina",hashParser(values));
+} 
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
