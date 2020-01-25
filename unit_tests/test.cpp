1b1a02b527378a131d14a3a95179df9c149775fc
Syris Norelli <snore001@ucr.edu>
2020-01-24 18:38:07 -0800

    Corrected fix

--- unit_tests/test.cpp
+++ unit_tests/test.cpp
@@ -4,7 +4,13 @@
 #include <vector>
 #include <iostream>
 
+ TEST (hashParseTest, FirstCheck)
  {
+ 	vector <string> values;
+ 	values.push_back("hamina");
+ 	values.push_back("#");
+	values.push_back("hello");
+	vector<string> output;
 	output.push_back("hamina");	
  	EXPECT_EQ(output,hashParser(values));
  } 
@@ -16,7 +22,30 @@ TEST (splitSpaces, singleSpace) {
 	vector<string> outputV = splitSpaces("abc 123");
 	EXPECT_EQ(values, outputV);
 }
-	
+
+TEST (splitSpaces, flankingSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("              abc 123              ");
+	EXPECT_EQ(values, outputV);
+}
+
+TEST (splitSpaces, multipleMiddleSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("abc             123");
+	EXPECT_EQ(values, outputV);
+}
+
+TEST (splitSpaces, allSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("             abc               123               ");
+	EXPECT_EQ(values, outputV);
+}
 
 
 int main(int argc, char **argv) {
