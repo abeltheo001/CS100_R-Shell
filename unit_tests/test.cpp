1d800421f651fcd8db7b6372651bcc1ee9075937
Abel Theodros <atheo003@hammer.cs.ucr.edu>
2020-01-25 14:00:59 -0800

    minor test update

---
+++ unit_tests/test.cpp
@@ -0,0 +1,51 @@
+#include "gtest/gtest.h"
+#include "../src/splitSpaces.h"
+// #include "../src/hashParser.h"
+#include <vector>
+#include <iostream>
+
+// TEST (hashParseTest, FirstCheck)
+// {
+// 	vector <string> values;
+// 	values.push_back("hamina");
+// 	values.push_back("#");	
+// 	EXPECT_EQ("hamina",hashParser(values));
+// } 
+
+TEST (splitSpaces, singleSpace) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("abc 123");
+	EXPECT_EQ(values, outputV);
+}
+	
+TEST (splitSpaces, flankingSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("              abc 123              ");
+	EXPECT_EQ(values, outputV);
+}
+
+TEST (splitSpaces, multipleMiddleSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("abc             123");
+	EXPECT_EQ(values, outputV);
+}
+
+TEST (splitSpaces, allSpaces) {
+	vector<string> values;
+	values.push_back("abc");
+	values.push_back("123");
+	vector<string> outputV = splitSpaces("             abc               123               ");
+	EXPECT_EQ(values, outputV);
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
