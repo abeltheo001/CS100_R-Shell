c928130aa3a702eeae485a886957025f5d147adf
Syris Norelli <snore001@ucr.edu>
2020-02-08 19:51:18 -0800

    Rename filterCommentsTest.h

--- unit_tests/test.cpp
+++ unit_tests/test.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 #include "splitSpacesTest.h"
-#include "filterComments.h"
+#include "filterCommentsTest.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
