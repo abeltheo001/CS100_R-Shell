1c9d9a8e75270f80c14523ec1955db6e3cbb22f0
Syris Norelli <snore001@ucr.edu>
2020-02-10 18:24:42 -0800

    Fix parser logic - commentFilter should be after groupQuotes

--- src/parser.cpp
+++ src/parser.cpp
@@ -11,11 +11,11 @@ CommandTree* parse(string userInput) {
 	// 1. Split on spaces
 	vector<string> words = splitOnChar(userInput, ' ');
 
-	// 2. Filter out #
-	words = filterComments(words);
-
-	// 3. Make sure quotes get grouped together
+	// 2. Make sure quotes get grouped together
 	words = groupQuotes(words)
+
+	// 3. Filter out #
+	words = filterComments(words);
 
 	// 4. Group words/quotes into Tokens of type Subcommand or Operator.
 	vector<Token*> tokens = tokenize(words);
