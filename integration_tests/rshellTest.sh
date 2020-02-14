b70a8a445f3e5077b32ba97056d7af59fae23fd7
Syris Norelli <snore001@ucr.edu>
2020-02-13 23:18:18 -0800

    Add bash connector integration test

--- integration_tests/rshellTest.sh
+++ integration_tests/rshellTest.sh
@@ -1,2 +1,21 @@
-OUTPUT="$(ls -a)"
-RSHELLOUTPUT="$(./rshell ls -a)"
+#!/bin/sh
+
+OUTPUT=$(ls)
+RSHELLOUTPUT=$(./rshell ls)
+
+if [ "$OUTPUT" == "$RSHELLOUTPUT" ]
+then
+	echo "rshell works as expected on ls."
+else
+	echo "rshell does not work as expected on ls."
+fi
+
+OUTPUTconnector=$(echo a || echo b && echo c)
+RSHELLOUTPUTconnector=$(./rshell echo a || echo b && echo c)
+
+if [ "$OUTPUTconnector" == "$RSHELLOUTPUTconnector" ]
+then
+	echo "rshell works as expected on connectors."
+else
+	echo "rshell does not work as expected on connectors."
+fi
