43554ba76b4c72ce68fb8e42dbe916d7a4d9a3a6
Abel Theodros <atheo003@ucr.edu>
2020-03-19 08:42:12 -0700

    Unit testing for >>

--- unit_tests/redirectionTest.h
+++ unit_tests/redirectionTest.h
@@ -19,27 +19,50 @@ TEST (redirectTest, Appending)
 	RShell shellobj = RShell();
 	string file = "input.txt";
 
-	Token* command = new Subcommand({file});
-	Token* response = new Subcommand({"DM the god himself"});
+	//Create inputs. 
+	Token* command = new Subcommand({"input.txt"});
+	Token* response = new Subcommand({"echo a"});
 	Token* check = new AppendOutToken({">>"});
 
-
+	//Store inputs into deque
 	deque<Token*> V;
-	V.push_back(command);
 	V.push_back(response);
+	V.push_back(command);
 	V.push_back(check);
 
+	//Run the shell
 	shellobj.commandDeque = V;
 	shellobj.shuntingExecute(V);
 	
-	string checker;
+	//Open the file 
 	ifstream in;
 	in.open(file);
-	getline(in,checker);
 
+	//Check bottom of file for latest input
+	//Iterator to check bottom row of file based on code from stackoverflow
+	//https://stackoverflow.com/questions/11876290/c-fastest-way-to-read-only-last-line-of-text-file
+	if (in.is_open()){	
+		char x; 
+		bool check = true;
+		while(check) {
+		if ((int)in.tellg() <= 1) {
+			in.seekg(0);
+			check = false;
+		} else if (x == '\n') {
+			check = false;
+		} else {
+			in.seekg(-2, ios_base::cur);
+		} }
+
+	}	
+
+	//Once at the lowest point in the file, grab the string and check if it's correct.
+	string end;
+	getline (in,end);
+	in.close();	
 	
 
-	EXPECT_EQ(checker,"DM the god himself");
+	EXPECT_EQ(end,"a");
 }
 
 TEST (redirectTest, Wipe)
